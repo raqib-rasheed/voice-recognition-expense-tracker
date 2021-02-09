aa24e83a7797d77bb907f35d52318ac44a8e4a62
raqib-rasheed <raqib.abdulrasheed@outlook.com>
2021-02-10 00:07:05 +0530

    started updating chart component

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,12 +1,34 @@
 import { Col } from "antd";
 import React, { useContext } from "react";
-import { ExportTrackerContext } from "../../context/Context";
+import { Doughnut } from "react-chartjs-2";
+import {
+  incomeCategories,
+  expenseCategories,
+} from "../../utils/categoriesConstants";
 
 export type PropVal = {
   title: string;
 };
 
 const Chart = (props: PropVal) => {
+  const { title } = props;
+  const categories = title === "income" ? incomeCategories : expenseCategories;
+
+  const state = {
+    labels: categories.map((el) => el.type),
+    datasets: [
+      {
+        label: "bytes of Code",
+        backgroundColor: ["#C9DE00", "#2FDE00", "#00A6B4", "#6800B4"],
+        hoverBackgroundColor: ["#4B5000", "#003350", "#35014F", "#175000"],
+        fill: false,
+        borderColor: "rgba(0,0,0,.45)",
+        borderWidth: 2,
+        data: categories.map((el) => el.amount),
+      },
+    ],
+  };
+
   return (
     <>
       <Col
@@ -15,6 +37,7 @@ const Chart = (props: PropVal) => {
         lg={{ span: 6, offset: 2 }}
       >
         <h4>{props.title}</h4>
+        <Doughnut data={state} />
       </Col>
     </>
   );
